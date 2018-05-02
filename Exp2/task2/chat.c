3cbeb2181593b0fb14e308e4eeae2bfb35756905
PillVic <pillaptx4869@163.com>
2018-05-02 19:27:54 +0800

    chat frame done

--- Exp2/task2/chat.c
+++ Exp2/task2/chat.c
@@ -2,16 +2,31 @@
 #include<sys/types.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+
 
 int main(){
     pid_t id;
-    if((id=vfork())==0){
+    if((id=fork())==0){
 	//Child Process
-	printf("Child:Fuck you  \n");
+	printf("Child Process Created..\n");
+	while(1){
+	    char msg[100];
+	    printf("Child:  ");
+	    scanf("%s",msg);
+	    printf("\n");
+	}
 	exit(0);
     }else{
 	//Father Process
-	printf("Father:Come on!    \n");
+	printf("Father Process Created..\n");
+	while(1){
+	    char msg[100]; 
+	    printf("Father: ");
+	    scanf("%s",msg);
+	    printf("\n");
+	}
 	exit(0);
     }
     return 0;
